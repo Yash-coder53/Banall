36f1ccedc04a5ec87256d8d89add5c6e1da53fcd
Sikander <139198327+Yash-coder53@users.noreply.github.com>
2025-02-11 20:47:28 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler
+
+logging.basicConfig(level=logging.INFO)
+
+TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+
+    dispatcher = updater.dispatcher
+
+    # Handlers
+    start_handler = CommandHandler('start', start_callback)
+    banall_handler = CommandHandler('banall', banall_callback)
+
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(banall_handler)
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
