f906ce644978d4af4984a16da1e676aefaa9e921
Sikander <139198327+Yash-coder53@users.noreply.github.com>
2025-02-11 21:10:06 +0530

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,9 @@
+from .ban_all import ban_all_handler
+from .start import start_handler
+from .help import help_handler
+
+Register all handlers here
+handlers = [
+    ban_all_handler,
+    start_handler,
+    help_handler
