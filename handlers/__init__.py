90cb91a93cc296ab2b2a5450045296bd6e4998f7
Sikander <139198327+Yash-coder53@users.noreply.github.com>
2025-03-10 13:43:12 +0530

    Update __init__.py
    Signed-off-by: Sikander <139198327+Yash-coder53@users.noreply.github.com>

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,7 +2,6 @@ from Slayer.ban_all import ban_all_handler
 from Slayer.start import start_handler
 from Slayer.help import help_handler
 
-Register all handlers here
 handlers = [
     CommandHandler('/banall',ban_all_handler),
     CommandHandler('/start',start_handler),
